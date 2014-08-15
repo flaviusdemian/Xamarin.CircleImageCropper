fab966f0709b09b2420eb6d588c04c54f391c7a9
Demian Flavius <flaviusdemian91@yahoo.com>
2014-08-15 23:54:33 +0300

    removed the java code

--- android demo/CircleImageCropper-master/CircleImageCropper-master/src/com/edmodo/cropper/cropwindow/edge/EdgeType.java
+++
@@ -1,9 +0,0 @@
-package com.edmodo.cropper.cropwindow.edge;
-
-public class EdgeType 
-{
-	final static int LEFT = 0;
-	final static int TOP = 1;
-	final static int RIGHT = 2;
-	final static int BOTTOM = 3;
-}
